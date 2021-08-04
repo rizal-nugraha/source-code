a3292ef84ffb2f5208d15224190daab9b3bad5f2
Rizal Nugraha <rizal.nugrah@gmail.com>
2021-08-04 21:36:18 +0700

    Membuat Hello World dengan Toast

--- 1.Hello World/app/src/main/java/id/kotlin/helloworld/MainActivity.kt
+++ 1.Hello World/app/src/main/java/id/kotlin/helloworld/MainActivity.kt
@@ -1,6 +1,8 @@
 package id.kotlin.helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
@@ -8,6 +10,10 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_ku)
+        button.setOnClickListener { Toast.makeText( this, "hello World", Toast.LENGTH_SHORT).show()}
+
     }
 
 
