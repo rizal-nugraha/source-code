439861f3c238ff5b4a45cf93c14dab6c3fad00dd
Rizal Nugraha <rizal.nugrah@gmail.com>
2021-08-04 22:03:42 +0700

    Membuat Hello World dengan Snackbar

--- 1.Hello World/app/src/main/java/id/kotlin/helloworld/MainActivity.kt
+++ 1.Hello World/app/src/main/java/id/kotlin/helloworld/MainActivity.kt
@@ -2,8 +2,10 @@ package id.kotlin.helloworld
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity: AppCompatActivity() {
 
@@ -11,8 +13,13 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val goodlayout = findViewById<LinearLayout>(R.id.good_layout)
+        val btnSnack = findViewById<Button>(R.id.btn_ku_snack)
+        btnSnack.setOnClickListener { Snackbar.make(goodlayout, "hello world versi Snack", Snackbar.LENGTH_SHORT).show() }
+
         val button = findViewById<Button>(R.id.btn_ku)
         button.setOnClickListener { Toast.makeText( this, "hello World", Toast.LENGTH_SHORT).show()}
+
 
     }
 
